e3ba1527bfe7a104d58015123fd2aaccb0906dfa
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2015-04-26 22:18:49 -0400

    Get Ajax to return array ooo-yeah

--- d3 version/index_d3.php
+++ d3 version/index_d3.php
@@ -69,56 +69,6 @@ function showResponse (response) {
 
 <hr />
 
-        <script type="text/javascript">
-
-            //Width and height
-            var w = 300;
-            var h = 300;
-
-            var dataset = [];
-
-            var outerRadius = w / 2;
-            var innerRadius = 0;
-            var arc = d3.svg.arc()
-                            .innerRadius(innerRadius)
-                            .outerRadius(outerRadius);
-            
-            var pie = d3.layout.pie();
-            
-            //Easy colors accessible via a 10-step ordinal scale
-            var color = d3.scale.category10();
-
-            //Create SVG element
-            var svg = d3.select("body")
-                        .append("svg")
-                        .attr("width", w)
-                        .attr("height", h);
-            
-            //Set up groups
-            var arcs = svg.selectAll("g.arc")
-                          .data(pie(dataset))
-                          .enter()
-                          .append("g")
-                          .attr("class", "arc")
-                          .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")");
-            
-            //Draw arc paths
-            arcs.append("path")
-                .attr("fill", function(d, i) {
-                    return color(i);
-                })
-                .attr("d", arc);
-            
-            //Labels
-            arcs.append("text")
-                .attr("transform", function(d) {
-                    return "translate(" + arc.centroid(d) + ")";
-                })
-                .attr("text-anchor", "middle")
-                .text(function(d) {
-                    return d.value;
-                });
-                        
-        </script>
+        
     </body>
 </html>
--- d3 version/scriptphrase_d3.js
+++ d3 version/scriptphrase_d3.js
@@ -1,5 +1,70 @@
 $(document).ready(function () {
-	$('#phrase-lookup').submit(function(e){
+
+// function processData(data) {
+//   var myData = [];
+
+//   var myDates = [];
+//   var results = [];
+//   // var medTemps = ['Median Temperature'];
+//   // var meanPress = ['Mean Pressure'];
+//   // var medPress = ['Median Pressure'];
+//   // var meanSpeeds = ['Mean Speed'];
+//   // var medSpeeds = ['Median Speed'];
+
+//   for (var key in data) {
+//     if (data.hasOwnProperty(key)) {
+//         results.push(data[0]);
+//         results.push(data[1]);
+//         // meanPress.push(getMean(data[key].p));
+//         // medPress.push(getMedian(data[key].p));
+//         // meanSpeeds.push(getMean(data[key].s));
+//         // medSpeeds.push(getMedian(data[key].s));
+//     } // hasOwnProperty
+//   } // for key in data
+
+//   myData.push(results);
+//   return myData;
+// } // Process Data
+
+function processData(data) {
+	var myData = [];
+
+	for (var key in data) {
+	   if (data.hasOwnProperty(key)) {
+	   	myData.push(data[key]);
+	   }
+	}
+
+	return myData;
+};
+
+function crunchData(data) {
+	for (var i = 0; i < data.length; i++) {
+  	var current = data[i];
+  	// console.log(current);
+  	return current;
+  }
+};
+
+
+function smashData(data) {
+	var partyResult = [];
+	var countResult = [];
+	for (var i = 0; i < data.length; i++) {
+  	var count = data[i].count;
+  	var party = data[i].party;
+  	console.log(count);
+  	console.log(party);
+  	// return count;
+  	// return party;
+  	partyResult.push(party);
+  	countResult.push(count);
+  }
+  console.log(partyResult);
+  console.log(countResult);
+};
+
+$('#phrase-lookup').submit(function(e){
 		e.preventDefault();
 
 var query_params = { apikey: 'a059153222414415a40f0b667f0b57ed',
@@ -16,7 +81,7 @@ var options = {
   dataType: 'jsonp'      
 };
 
-var request = jQuery.ajax(endpoint, options).done(showResponse, showResponse1, showResponse2);
+var request = jQuery.ajax(endpoint, options).done(showResponse, showResponse1, showResponse2, showResponse3, showResponse4, showResponse5);
 
 function showResponse1 (response) {
 	console.log(response);
@@ -26,6 +91,23 @@ function showResponse1 (response) {
 // experiment with localStorage - not essential functionality
 function showResponse2 (response) {
 localStorage.setItem('response', JSON.stringify(response));
+};
+
+function showResponse3(response) {
+      var processed = processData(response);
+      console.log(processed);
+      return processed;
+};
+
+function showResponse4(response) {
+	var crunch = crunchData(processData(response));
+	console.log(crunch);
+	return crunch;
+};
+
+function showResponse5(response) {
+	var smash = smashData(crunchData(processData(response)));
+	console.log(smash);
 };
 
 });
