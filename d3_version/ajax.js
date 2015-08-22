89b1d205dcf2465b1feac6628df972ebfca8bd76
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2015-08-22 18:38:15 -0400

    correctly total up party affiliation :train:

--- d3_version/ajax.js
+++ d3_version/ajax.js
@@ -18,48 +18,103 @@ var options = {
   dataType: 'jsonp'      
 };
 
-var request = $.ajax(endpoint, options).done(showResponse, showResponse1, showResponse2);
+var request = $.ajax(endpoint, options).done(showResponse1, showResponse2);
 
 function showResponse1 (response) {
   console.log(response);
-
 };
 
-function processData(data) {
-	var myData = [];
+// function processData(data) {
+// 	var myData = [];
+// 	for (var key in data) {
+// 	   if (data.hasOwnProperty(key)) {
+// 	   	myData.push(data[key]);
+// 	   }
+// 	}
+//   console.log(myData);
+// 	return myData;
+// };
 
-	for (var key in data) {
-	   if (data.hasOwnProperty(key)) {
-	   	myData.push(data[key]);
-	   }
-	}
-  console.log(myData);
-	return myData;
-};
+// function crunchData(data) {
+// 	for (var i = 0; i < data.length; i++) {
+//   	var current = data[i];
+//   	console.log(current);
+//   	return current;
+//   }
+// };
 
-function crunchData(data) {
-	for (var i = 0; i < data.length; i++) {
-  	var current = data[i];
-  	console.log(current);
-  	return current;
-  }
-};
 
-function convertParty(data){
-  
-  if (data.party == "R" || data.party == "Republican") {
-    data.party = "Republican";
-  } else if (data.party == "D" || data.party == "Democrat") {
-    data.party = "Democrat";
-  } else {
-    data.party = "Independent";
-  }
+function generateChart(dataset) {
 
-};
+console.log(dataset);
 
-function generateChart(data) {
+var myArray = dataset.results;
 
-  data.forEach(convertParty);
+          function convertParty(item){
+            
+            if (item.party == "R" || item.party == "Republican") {
+              item.party = "Republican";
+            } else if (item.party == "D" || item.party == "Democrat") {
+              item.party = "Democrat";
+            } else if (item.party = "ID" || item.party == "Independent") {
+              item.party = "Independent";
+            } else {
+              item.party = "N/A"
+            }
+
+          };
+
+          myArray.forEach(convertParty);
+
+          function republicans(item) {
+            return item.party == "Republican";
+          }
+
+          //total up republicans
+          var filterRepublicans = myArray.filter(republicans);
+
+          var totalRepublicans=0;
+          for (var i in filterRepublicans) { 
+            totalRepublicans += filterRepublicans[i].count; 
+          };
+
+          var republican = {party: "Republicans", count: 0};
+          republican.count = totalRepublicans;
+
+
+          //total up democrats
+          function democrats(item) {
+            return item.party == "Democrat";
+          };
+
+          var filterDemocrats = myArray.filter(democrats);
+
+          var totalDemocrats=0;
+          for (var i in filterDemocrats) { 
+            totalDemocrats += filterDemocrats[i].count; 
+          };
+
+          var democrat = {party: "Democrats", count: 0};
+          democrat.count = totalDemocrats;
+
+          //total up independents
+          function independents(item) {
+            return item.party == "Independent";
+          };
+
+          var filterIndependents = myArray.filter(independents);
+
+          var totalIndependents=0;
+          for (var i in filterIndependents) { 
+            totalIndependents += filterIndependents[i].count; 
+          };
+
+          var independent = {party: "Independents", count: 0};
+          independent.count = totalIndependents;
+
+          var data = [];
+          data.push(democrat, republican, independent);
+
 
       var width = 960,
           height = 500,
@@ -103,7 +158,7 @@ function generateChart(data) {
 }
 
 function showResponse2(response) {
-  var pieGenerated = generateChart(crunchData(processData(response)));
+  var pieGenerated = generateChart(response);
 };
 
 });
