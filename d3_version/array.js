7c36bcf2df9acfc7423787db7236be1c530dec7f
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2015-08-22 18:10:54 -0400

    total up parties

--- d3_version/array.js
+++ d3_version/array.js
@@ -1,4 +1,4 @@
-var array = [{count: 147, party: "R"}, {count: 147, party: "Republican"}, {count: 130, party: "D"}, count: 100, party: "Democrat"}, {count: 4, party: "ID"}];
+var myArray = [{count: 190, party: "R"}, {count: 147, party: "Republican"}, {count: 130, party: "D"}, {count: 100, party: "Democrat"}, {count: 4, party: "ID"}];
 
 function convertParty(item){
 	
@@ -12,4 +12,39 @@ function convertParty(item){
 
 };
 
-array.forEach(convertParty);
+myArray.forEach(convertParty);
+
+function republicans(item) {
+  return item.party == "Republican";
+}
+
+//total up republicans
+var filterRepublicans = myArray.filter(republicans);
+
+var totalRepublicans=0;
+for (var i in filterRepublicans) { 
+	totalRepublicans += filterRepublicans[i].count; 
+};
+
+var republican = {party: "Republicans", count: 0};
+republican.count = totalRepublicans;
+
+
+//total up democrats
+function democrats(item) {
+  return item.party == "Democrat";
+};
+
+var filterDemocrats = myArray.filter(democrats);
+
+var totalDemocrats=0;
+for (var i in filterDemocrats) { 
+	totalDemocrats += filterDemocrats[i].count; 
+};
+
+var democrat = {party: "Democrats", count: 0};
+democrat.count = totalDemocrats;
+
+var data = [];
+data.push(democrat, republican);
+
